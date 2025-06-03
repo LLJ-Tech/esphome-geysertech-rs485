256524cd32d307a4cf3ee04de28181b9c67a33ef
LLJ-Tech <69767865+LLJ-Tech@users.noreply.github.com>
2025-06-03 21:06:01 +0200

    Update geysertech_rs485.h

--- components/geysertech_rs485/geysertech_rs485.h
+++ components/geysertech_rs485/geysertech_rs485.h
@@ -5,6 +5,10 @@ class GeyserTechRS485 : public PollingComponent {
  public:
   Sensor *geyser_temp_sensor = nullptr;
   Sensor *pv_voltage_sensor = nullptr;
+  Sensor *pv_current_sensor = nullptr;
+  Sensor *grid_voltage_sensor = nullptr;
+  Sensor *pv_power_sensor = nullptr;
+  Sensor *solar_power_kwh_sensor = nullptr;
   TextSensor *geyser_status_sensor = nullptr;
 
   GeyserTechRS485() : PollingComponent(1000) {}
@@ -14,5 +18,9 @@ class GeyserTechRS485 : public PollingComponent {
 
   void set_geyser_temp_sensor(Sensor *sensor) { geyser_temp_sensor = sensor; }
   void set_pv_voltage_sensor(Sensor *sensor) { pv_voltage_sensor = sensor; }
+  void set_pv_current_sensor(Sensor *sensor) { pv_current_sensor = sensor; }
+  void set_grid_voltage_sensor(Sensor *sensor) { grid_voltage_sensor = sensor; }
+  void set_pv_power_sensor(Sensor *sensor) { pv_power_sensor = sensor; }
+  void set_solar_power_kwh_sensor(Sensor *sensor) { solar_power_kwh_sensor = sensor; }
   void set_geyser_status_sensor(TextSensor *sensor) { geyser_status_sensor = sensor; }
 };
