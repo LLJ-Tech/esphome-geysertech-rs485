60ac21b0d9afa9314dc4d28220e471c5e1d517c2
LLJ-Tech <69767865+LLJ-Tech@users.noreply.github.com>
2025-06-03 21:07:42 +0200

    Update sensor.py

--- components/geysertech_rs485/sensor.py
+++ components/geysertech_rs485/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, text_sensor
-from esphome.const import CONF_ID, UNIT_CELSIUS, UNIT_VOLT, ICON_THERMOMETER
+from esphome.const import CONF_ID, UNIT_CELSIUS, UNIT_VOLT, UNIT_AMPERE, UNIT_WATT, UNIT_KILOWATT_HOUR, ICON_THERMOMETER, ICON_POWER
 
 DEPENDENCIES = ['uart']
 geysertech_rs485_ns = cg.esphome_ns.namespace('geysertech_rs485')
@@ -18,6 +18,23 @@ CONFIG_SCHEMA = cv.Schema({
         unit_of_measurement=UNIT_VOLT,
         accuracy_decimals=1
     ),
+    cv.Optional('pv_current'): sensor.sensor_schema(
+        unit_of_measurement=UNIT_AMPERE,
+        accuracy_decimals=1
+    ),
+    cv.Optional('grid_voltage'): sensor.sensor_schema(
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=1
+    ),
+    cv.Optional('pv_power'): sensor.sensor_schema(
+        unit_of_measurement=UNIT_WATT,
+        icon=ICON_POWER,
+        accuracy_decimals=1
+    ),
+    cv.Optional('solar_power_kwh'): sensor.sensor_schema(
+        unit_of_measurement=UNIT_KILOWATT_HOUR,
+        accuracy_decimals=3
+    ),
     cv.Optional('geyser_status'): text_sensor.text_sensor_schema(),
 }).extend(cv.polling_component_schema('1s')).extend(cv.COMPONENT_SCHEMA)
 
@@ -31,6 +48,18 @@ async def to_code(config):
     if 'pv_voltage' in config:
         sens = await sensor.new_sensor(config['pv_voltage'])
         cg.add(var.set_pv_voltage_sensor(sens))
+    if 'pv_current' in config:
+        sens = await sensor.new_sensor(config['pv_current'])
+        cg.add(var.set_pv_current_sensor(sens))
+    if 'grid_voltage' in config:
+        sens = await sensor.new_sensor(config['grid_voltage'])
+        cg.add(var.set_grid_voltage_sensor(sens))
+    if 'pv_power' in config:
+        sens = await sensor.new_sensor(config['pv_power'])
+        cg.add(var.set_pv_power_sensor(sens))
+    if 'solar_power_kwh' in config:
+        sens = await sensor.new_sensor(config['solar_power_kwh'])
+        cg.add(var.set_solar_power_kwh_sensor(sens))
     if 'geyser_status' in config:
         sens = await text_sensor.new_text_sensor(config['geyser_status'])
         cg.add(var.set_geyser_status_sensor(sens))
